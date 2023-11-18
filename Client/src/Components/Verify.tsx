7f717dfafe9123f11bac15a0bebac755c59e8c63
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-11-18 15:40:25 +0530

    Components Added

--- Client/src/Components/Verify.tsx
+++ Client/src/Components/Verify.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { Center, Heading } from '@chakra-ui/react'
+import { Center, Heading } from "@chakra-ui/react";
 import {
   Button,
   FormControl,
@@ -9,45 +9,59 @@ import {
   Stack,
   useColorModeValue,
   HStack,
-} from '@chakra-ui/react'
-import { PinInput, PinInputField } from '@chakra-ui/react'
+} from "@chakra-ui/react";
+import { PinInput, PinInputField } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Verify() {
+  const [isOtp, setIsOtp] = useState("");
+  const handleVerify = () => {};
+
   return (
     <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
       <Stack
         spacing={4}
-        w={'full'}
-        maxW={'sm'}
-        bg={useColorModeValue('white', 'gray.700')}
-        rounded={'xl'}
-        boxShadow={'lg'}
+        w={"full"}
+        maxW={"sm"}
+        bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        boxShadow={"lg"}
         p={6}
-        my={10}>
+        my={10}
+      >
         <Center>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-            Verify your Email
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
+            Verify your Mobile
           </Heading>
         </Center>
         <Center
-          fontSize={{ base: 'sm', sm: 'md' }}
-          color={useColorModeValue('gray.800', 'gray.400')}>
-          We have sent code to your email
+          fontSize={{ base: "sm", sm: "md" }}
+          color={useColorModeValue("gray.800", "gray.400")}
+        >
+          We have sent code to your Mobile
         </Center>
         <Center
-          fontSize={{ base: 'sm', sm: 'md' }}
+          fontSize={{ base: "sm", sm: "md" }}
           fontWeight="bold"
-          color={useColorModeValue('gray.800', 'gray.400')}>
+          color={useColorModeValue("gray.800", "gray.400")}
+        >
           username@mail.com
         </Center>
         <FormControl>
           <Center>
             <HStack>
-              <PinInput>
+              <PinInput
+                placeholder=""
+                value={isOtp}
+                onChange={(value) => setIsOtp(value)}
+              >
+                <PinInputField />
+                <PinInputField />
                 <PinInputField />
                 <PinInputField />
                 <PinInputField />
@@ -58,15 +72,17 @@ export default function Verify() {
         </FormControl>
         <Stack spacing={6}>
           <Button
-            bg={'blue.400'}
-            color={'white'}
+            onClick={handleVerify}
+            bg={"blue.400"}
+            color={"white"}
             _hover={{
-              bg: 'blue.500',
-            }}>
+              bg: "blue.500",
+            }}
+          >
             Verify
           </Button>
         </Stack>
       </Stack>
     </Flex>
-  )
+  );
 }
