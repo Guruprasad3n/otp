343cde906da9e6e0a2b8aa5feef4eb03b596a6fd
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-11-20 13:13:47 +0530

    Errors Solved

--- Client/src/Components/Verify.tsx
+++ Client/src/Components/Verify.tsx
@@ -18,22 +18,22 @@ import {
 import axios from "axios";
 import Cookies from "js-cookie";
 import { useState } from "react";
-
-export default function Verify() {
-  const [isOtp, setIsOtp] = useState("");
+type VerifyProps = {};
+const Verify: React.FC<VerifyProps> = () => {
+  const [isOtp, setIsOtp] = useState<number>(0); // Assuming the initial value should be 0 as a number
   const toast = useToast();
 
+  const mobileNumber = Cookies.get("mobileNumber");
   const handleVerify = async () => {
     try {
-      // const mobileNumber = Cookies.get("mobileNumber");
       const storedMobileNumber = localStorage.getItem("mobileNumber");
       console.log("Mobile Number:", storedMobileNumber);
       console.log("Entered OTP:", isOtp);
 
-      const response = await axios.post(
+      const response = await axios.post<{ success: boolean }>(
         "http://localhost:3000/api/otp/verify",
         {
-          identifier: storedMobileNumber,
+          identifier: mobileNumber,
           userEnteredOTP: isOtp,
         }
       );
@@ -72,7 +72,7 @@ export default function Verify() {
       const storedMobileNumber = Cookies.get("mobileNumber");
       console.log(storedMobileNumber);
       await axios.post("http://localhost:3000/api/otp/generate", {
-        identifier: `${storedMobileNumber}`,
+        identifier: `${mobileNumber}`,
       });
       toast({
         title: "OTP Resent",
@@ -119,17 +119,15 @@ export default function Verify() {
           fontWeight="bold"
           color={useColorModeValue("gray.800", "gray.400")}
         >
-          {/* <span style={{ fontWeight: "400" }}>Your Mobile Number </span>{" "} */}
           {localStorage.getItem("mobileNumber")}
-          {/* username@mail.com */}
         </Center>
         <FormControl>
           <Center>
             <HStack>
               <PinInput
                 placeholder=""
-                value={isOtp}
-                onChange={(value) => setIsOtp(value)}
+                value={isOtp.toString()}
+                onChange={(value) => setIsOtp(Number(value))}
               >
                 <PinInputField />
                 <PinInputField />
@@ -143,16 +141,11 @@ export default function Verify() {
         </FormControl>
         <Container display={"flex"} justifyContent={"end"}>
           <Text
-            // style={{cursor: 'pointer'}}
-
             paddingX={2}
             onClick={handleResend}
             fontWeight={"600"}
-            // w={'40%'}
-            // variant={"outline"}
             _hover={{
               cursor: "pointer",
-              // bg: "blue.500",
               color: "blue.400",
             }}
           >
@@ -174,149 +167,6 @@ export default function Verify() {
       </Stack>
     </Flex>
   );
-}
+};
 
-// ========================================================
-
-// "use client";
-
-// import {
-//   Center,
-//   Heading,
-//   useToast,
-//   Button,
-//   FormControl,
-//   Flex,
-//   Stack,
-//   useColorModeValue,
-//   HStack,
-//   PinInput,
-//   PinInputField,
-// } from "@chakra-ui/react";
-// import axios from "axios";
-// import Cookies from "js-cookie";
-// import { useState } from "react";
-
-// export default function Verify() {
-//   const [isOtp, setIsOtp] = useState("");
-//   const toast = useToast();
-//   const handleVerify = async () => {
-//     try {
-//       const mobileNumber = Cookies.get("mobileNumber");
-//       console.log(mobileNumber);
-//       const response = await axios.post(
-//         "http://localhost:3000/api/otp/verify",
-//         {
-//           identifier: `${mobileNumber}`, // Replace with the actual identifier used during OTP generation
-//           userEnteredOTP: isOtp,
-//         }
-//       );
-//       console.log(identifier);
-
-//       if (response.data.success) {
-//         toast({
-//           title: "Verification Success",
-//           status: "success",
-//           duration: 3000,
-//           isClosable: true,
-//         });
-//       } else {
-//         toast({
-//           title: "Verification Failed",
-//           status: "warning",
-//           duration: 3000,
-//           isClosable: true,
-//         });
-//       }
-//     } catch (error) {
-//       toast({
-//         title: "Verification Failed",
-//         status: "error",
-//         duration: 3000,
-//         isClosable: true,
-//       });
-//       console.error("Error verifying OTP:", error);
-//     }
-//   };
-
-//   return (
-//     <Flex minH={"100vh"} align={"center"} justify={"center"} bg={"#242424"}>
-//       <Stack
-//         spacing={4}
-//         w={"full"}
-//         maxW={"sm"}
-//         bg={useColorModeValue("white", "gray.700")}
-//         rounded={"xl"}
-//         boxShadow={"lg"}
-//         p={6}
-//         my={10}
-//       >
-//         <Center>
-//           <Heading lineHeight={1.1} fontSize={{ base: "2xl", md: "3xl" }}>
-//             Verify your Mobile
-//           </Heading>
-//         </Center>
-//         <Center
-//           fontSize={{ base: "sm", sm: "md" }}
-//           color={useColorModeValue("gray.800", "gray.400")}
-//         >
-//           We have sent code to your Mobile
-//         </Center>
-//         <Center
-//           fontSize={{ base: "sm", sm: "md" }}
-//           fontWeight="bold"
-//           color={useColorModeValue("gray.800", "gray.400")}
-//         >
-//           username@mail.com
-//         </Center>
-//         <FormControl>
-//           <Center>
-//             <HStack>
-//               <PinInput
-//                 placeholder=""
-//                 value={isOtp}
-//                 onChange={(value) => setIsOtp(value)}
-//               >
-//                 <PinInputField />
-//                 <PinInputField />
-//                 <PinInputField />
-//                 <PinInputField />
-//                 <PinInputField />
-//                 <PinInputField />
-//               </PinInput>
-//             </HStack>
-//           </Center>
-//         </FormControl>
-//         <Stack spacing={6}>
-//           <Button
-//             onClick={handleVerify}
-//             bg={"blue.400"}
-//             color={"white"}
-//             _hover={{
-//               bg: "blue.500",
-//             }}
-//           >
-//             Verify
-//           </Button>
-//         </Stack>
-//       </Stack>
-//     </Flex>
-//   );
-// }
-
-// const identifier = 'your_identifier'; // replace with the actual identifier used during OTP generation
-// const response = await axios.post('/api/verifyOtp', {
-//   identifier,
-//   userEnteredOTP:isOtp,
-// });
-
-// setVerificationStatus({
-//   success: response.data.success,
-//   message: response.data.message,
-// });
-// } catch (error) {
-// console.error('Error verifying OTP:', error);
-// setVerificationStatus({
-//   success: false,
-//   message: 'An error occurred during OTP verification.',
-// });
+export default Verify;
